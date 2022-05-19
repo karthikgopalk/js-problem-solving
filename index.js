1af268d43098c3d9d0ddb8b31729e8d062f8d0a8
karthikgopalk <riokarthy@gmail.com>
2022-05-19 11:48:23 +0530

    2 files modified

--- index.js
+++ index.js
@@ -55,3 +55,53 @@ mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy */
 // console.log(areaOfTriangle(5, 6, 7));
 
 /*5. Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front.  */
+
+/*6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.  */
+
+// function leapYear(year) {
+//   return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+// }
+// console.log(leapYear(2021));
+
+/*7. Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050.  */
+
+// for (let i = 2014; i < 2050; i++) {
+//   let date = new Date(i.toString(), 0, 1);
+//   if (date.getDay() == 0) console.log(i);
+// }
+
+/*8. Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched".  */
+
+// let fixed = Math.trunc(Math.random() * 1e1);
+// fixed = fixed === 0 ? 1 : fixed;
+// let input = +prompt('enter the number', 0);
+// let output = input === fixed ? 'Good work' : 'Not Matched';
+// console.log('input: ' + input + ' generated: ' + fixed + ' output: ' + output);
+
+/*9. Write a JavaScript program to calculate days left until next Christmas. */
+//date -> cristmas = 25 dec -> cristmas-date
+
+// let date = new Date();
+// let Christmas = new Date(date.getFullYear(), 11, 25);
+// let oneDay = 1000 * 60 * 60 * 24;
+// let totalDays = Math.ceil((Christmas.getTime() - date.getTime()) / oneDay);
+// console.log(totalDays);
+
+/*11. Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
+[ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in Fahrenheit ]
+Expected Output :
+60°C is 140 °F
+45°F is 7.222222222222222°C */
+
+// function ctof(c) {
+//   return `the ${c}° c is ${(c * 9) / 5 + 32} f`;
+// }
+// function ftoc(f) {
+//   return `the ${f}° f is ${((f - 32) * 5) / 9} c`;
+// }
+// console.log(ctof(60));
+// console.log(ftoc(45));
+
+/*12. Write a JavaScript program to get the website URL (loading page).  */
+
+// console.log(document.URL);
