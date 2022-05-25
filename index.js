be784733718915dab1af4084b7bbdd5cc7c67807
karthikgopalk <riokarthy@gmail.com>
2022-05-25 21:11:45 +0530

    1 file modified

--- index.js
+++ index.js
@@ -101,3 +101,15 @@
 
 // let arr = [10, 20, 30];
 // console.log(arr.reduce((acc, cur) => acc + cur));
+
+//70. Write a JavaScript program to rotate the elements left of a given array of integers of length 3.
+
+// let arr = [3, 4, 5];
+// console.log([arr[1], arr[2], arr[0]]);
+
+//71. Write a JavaScript program to check whether 1 appears in first or last position of a given array of integers. The array length must be greater or equal to 1.
+
+// function checkOne(arr) {
+//   return arr[0] == 1 || arr[arr.length - 1] == 1;
+// }
+// console.log(checkOne([]));
