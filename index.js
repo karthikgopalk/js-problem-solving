a46f73e6e193a72f1d4d5184909e86e4e501b0da
karthikgopalk <riokarthy@gmail.com>
2022-06-09 07:16:45 +0530

    1 file modified

---
+++ index.js
@@ -0,0 +1,15 @@
+//Running Sum of 1d Array
+
+/*
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+*/
+
+// function addNextInde
+// const nums = [1, 2, 3, 4];
+// for (let i = 0; i <= nums.length - 1; i++) {
+//   let temp = nums[i] + nums[i + 1];
+//   nums[i + 1] = temp;
+// }
+// console.log(nums);
